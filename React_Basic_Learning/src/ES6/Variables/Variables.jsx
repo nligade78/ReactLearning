c861a1cf66ee8a408cc7677144489d9090b1406a
nligade78 <nikhilligade22@gmail.com>
2024-07-17 17:52:49 +0530

    UnderStading varibales

---
+++ React_Basic_Learning/src/ES6/Variables/Variables.jsx
@@ -0,0 +1,21 @@
+// Before ES6 there was only way to define variables with (VAR) 
+//Now ES6 there are ways of defining variables var,let,const
+
+
+// Example  var x=5.6;
+// If use var outside of function, it's belongs to the global scope
+// If use var inside of a function , it's belongs to that function
+// If use var inside of a block, i.e a forr loop, the variable is still available outside of that block.
+// var has function scope, not a block scope.
+
+// let x=6.6
+//  let is the block scoped version of var, and is limited to the block (or expression) where it is defined.
+// If you use let inside a block ,i.e a foor loop,the variables is only available inside of that loop.
+// let has block scope.
+
+
+//const x=6.9
+//const is a variable that once it has been created,its value can never change
+//const has a block scope
+
+
