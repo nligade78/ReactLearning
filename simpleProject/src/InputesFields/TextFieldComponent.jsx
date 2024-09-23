73453d627719dff0f7d6f465595f69b9d09d71d6
nligade78 <nikhilligade22@gmail.com>
2024-09-23 11:36:50 +0530

    bright changes

--- simpleProject/src/InputesFields/TextFieldComponent.jsx
+++ simpleProject/src/InputesFields/TextFieldComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextField } from '@mui/material';
 
-const TextFieldComponent = ({ label, name, value, onChange }) => {
+const TextFieldComponent = ({ label, name, value, onChange, size = 'small' }) => {
   return (
     <TextField
       fullWidth
@@ -10,6 +10,17 @@ const TextFieldComponent = ({ label, name, value, onChange }) => {
       name={name}
       value={value}
       onChange={onChange}
+      size={size}
+      sx={{
+        fontSize: '0.75rem', // Smaller font size for the input
+        '& .MuiInputBase-input': {
+          fontSize: '0.75rem', // Smaller font size for text input
+          padding: '8px 12px', // Adjust padding
+        },
+        '& .MuiFormLabel-root': {
+          fontSize: '0.75rem', // Smaller font size for label
+        },
+      }}
     />
   );
 };
