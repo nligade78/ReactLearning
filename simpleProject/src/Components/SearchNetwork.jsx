61b0ccdb87a5b051809654e56dcc994c7cf78be6
nligade78 <nikhilligade22@gmail.com>
2024-10-15 17:15:00 +0530

    ss

--- simpleProject/src/Components/SearchNetwork.jsx
+++ simpleProject/src/Components/SearchNetwork.jsx
@@ -247,9 +247,9 @@ const handleSubmit = (e) => {
         ...formData.header.originalDetails,
         requestors:[
           {
-            firstname:'',
-            lastname:'',
-            email:''
+            firstname:user.name,
+            lastname:user.lastNem,
+            email:user.email
           }
         ]
       }
@@ -489,65 +489,3 @@ const toggleCollapse =()=>{
 export default SearchNetwork;
 
 
-----
-
-export const handleClear = (
-  setFormData, 
-  add_linkage, 
-  setNetworkData, 
-  setIsSubmitted, 
-  setConfidenceScoreEnable, 
-  setClearEnabled, 
-  apiRef, 
-  setShowCheckBox, 
-  setShowContractedState, 
-  setIsContractStateEnabled, 
-  setFormErrors, 
-  setLobLabel
-) => {
-  setFormData(add_linkage); // Reset form data
-  setNetworkData([]); // Clear data sent to NetworkTable
-  setIsSubmitted(false);
-  add_linkage.profile.masterProvID = "";
-  setConfidenceScoreEnable(false);
-  setClearEnabled(false);
-  apiRef.current.setFilterModel({ items: [] });
-  setShowCheckBox(false);
-  setShowContractedState(false);
-  setIsContractStateEnabled(false);
-  setFormErrors({});
-  setLobLabel({
-    masterProvIDLabel: "Org ID"
-  });
-};
-
-
-// Inside your component
-import { handleClear } from './networkHandlers'; // Ensure the correct path
-
-// ... inside your component function
-return (
-  // ... your JSX
-  <button
-    onClick={() =>
-      handleClear({
-        setFormData,
-        add_linkage,
-        setNetworkData,
-        setIsSubmitted,
-        setConfidenceScoreEnable,
-        setClearEnabled,
-        apiRef,
-        setShowCheckBox,
-        setShowContractedState,
-        setIsContractStateEnabled,
-        setFormErrors,
-        setLobLabel,
-      })
-    }
-  >
-    Clear
-  </button>
-  // ... rest of your  ...
-  
-);
